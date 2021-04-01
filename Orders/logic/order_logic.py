6e17c11986b2ead1e11cccdd88fa8c74f689d2d0
vrusi <me@vrusinkova.com>
2021-04-01 22:09:33 +0200

    Add update and delete order logic

--- Orders/logic/order_logic.py
+++ Orders/logic/order_logic.py
@@ -22,7 +22,7 @@ class OrderLogic:
 
         api_order = self.parser.parse_order(order)
         return api_order
-        
+
     def create_order(self, user, acc_id):
         try:
             Order.objects.create(
@@ -31,3 +31,24 @@ class OrderLogic:
             )
         except Exception as e:
             raise e
+
+    def update_order(self, user, order_id, data):
+        try:
+            order = Order.objects.filter(booked_by=user, id=order_id)[0]
+        except Exception as e:
+            raise e
+
+        order.acc_booked_id = data['acc_id']
+
+        order.save()
+
+        api_order = self.parser.parse_order(order)
+        return api_order
+
+    def delete_order(self, user, order_id):
+        try:
+            order = Order.objects.filter(booked_by=user, id=order_id)[0]
+        except Exception as e:
+            raise e
+
+        order.delete()
