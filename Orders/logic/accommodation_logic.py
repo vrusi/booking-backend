ca33ff2774eec59d875a553af4afb879015a2eb7
vrusi <me@vrusinkova.com>
2021-04-01 22:05:23 +0200

    Add get filtered accommodation

--- Orders/logic/accommodation_logic.py
+++ Orders/logic/accommodation_logic.py
@@ -4,6 +4,13 @@ from Orders.models import *
 class AccommodationLogic:
     parser = AccommodationParser()
 
+    def get_filtered_accommodations(self, filter_criteria):
+        accommodations = Accommodation.objects.all()
+
+        api_accommodations = [self.parser.parse_accommodation_overview(acc) for acc in accommodations]
+
+        return api_accommodations
+        
     def get_accommodation(self, pk):
         acc = Accommodation.objects.filter(id=pk)
 
