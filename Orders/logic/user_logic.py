f540f7a09c94709cb861ea0ef99d560a0e504455
vrusi <me@vrusinkova.com>
2021-04-01 22:11:34 +0200

    Add update user

--- Orders/logic/user_logic.py
+++ Orders/logic/user_logic.py
@@ -14,3 +14,17 @@ class UserLogic:
 
         api_user = self.parser.parser_user_info(user)
         return api_user
+
+    def update_user_info(self, user_id, data):
+
+        try:
+            user = UserProfile.objects.filter(id=user_id)[0]
+        except Exception as e:
+            raise e
+
+        user.first_name = data['firstName']
+        user.last_name = data['lastName']
+        user.save()
+
+        api_user = self.parser.parser_user_info(user)
+        return api_user
