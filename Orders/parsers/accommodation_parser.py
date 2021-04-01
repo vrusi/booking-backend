ea21020f25efaccbb23449b00432eb7ccbce6042
vrusi <me@vrusinkova.com>
2021-04-01 22:03:00 +0200

    Add methods for counting ratings

--- Orders/parsers/accommodation_parser.py
+++ Orders/parsers/accommodation_parser.py
@@ -27,3 +27,48 @@ class AccommodationParser:
         api_accommodation.rating.average /= api_accommodation.rating.count
 
         return api_accommodation
+
+    def _count_ratings(self, acc_rating, rating):
+        acc_rating.average += rating.rating
+        acc_rating.count += 1
+
+    def parse_and_count_ratings(self, object_rating, rating):
+        api_rating = api.Rating(
+            id=rating.id.__str__(),
+
+            author=api.User(
+                username=rating.user.username,
+                first_name=rating.user.first_name,
+                last_name=rating.user.last_name,
+            ),
+
+            rating=rating.rating,
+            title=rating.title,
+            content=rating.text,
+            created_at=rating.created_at.isoformat(),
+
+        )
+
+        file_name = f'rating_images/{rating.id.__str__()}'
+        f = open(file_name, 'rb')
+        api_rating.image = str(f.read())
+        f.close()
+
+
+        api_rating.replies = [api.RatingReply(
+            id=reply.id.__str__(),
+
+            author=api.User(
+                username=reply.user.username,
+                first_name=reply.user.first_name,
+                last_name=reply.user.last_name,
+            ),
+
+            content=reply.text,
+            created_at=reply.created_at,
+        ) for reply in RatingReply.objects.filter(rating_parent=rating)]
+
+        object_rating.average += rating.rating
+        object_rating.count += 1
+
+        return api_rating
