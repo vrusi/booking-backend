aa42a096884977f13fec2e15611c414eab75778f
vrusi <me@vrusinkova.com>
2021-04-01 22:08:48 +0200

    Add logic for getting orders

---
+++ Orders/logic/order_logic.py
@@ -0,0 +1,24 @@
+from Orders.models.models import *
+from Orders.parsers.order_parser import OrderParser
+
+
+class OrderLogic:
+    parser = OrderParser()
+
+    def get_list_orders(self, user):
+        try:
+            orders = Order.objects.filter(booked_by=user)
+        except Exception as e:
+            raise e
+
+        api_orders = [self.parser.parse_order(order) for order in orders]
+        return api_orders
+
+    def get_order(self, user, order_id):
+        try:
+            order = Order.objects.filter(booked_by=user, id=order_id)[0]
+        except Exception as e:
+            raise e
+
+        api_order = self.parser.parse_order(order)
+        return api_order
