f3faa59ce8b3171030be4e94493b80e55a5c1be3
vrusi <me@vrusinkova.com>
2021-04-01 22:06:41 +0200

    Add logic for accommodation ratings

--- Orders/logic/accommodation_logic.py
+++ Orders/logic/accommodation_logic.py
@@ -10,7 +10,7 @@ class AccommodationLogic:
         api_accommodations = [self.parser.parse_accommodation_overview(acc) for acc in accommodations]
 
         return api_accommodations
-        
+
     def get_accommodation(self, pk):
         acc = Accommodation.objects.filter(id=pk)
 
@@ -20,3 +20,44 @@ class AccommodationLogic:
         api_accommodation = self.parser.parse_accommodation_overview(acc[0])
 
         return api_accommodation
+
+    def create_rating(self, user, acc_id, data):
+        rating = Rating.objects.create(
+            accommodation_id=acc_id,
+            user=user,
+            rating=data['rating'],
+            title=data['title'],
+            text=data['text']
+        )
+
+        uploaded_file_name = data['image'].name.split('.')[-1]
+
+        file_name = f'rating_images/{rating.id.__str__()}'
+
+        f = open(file_name, 'wb')
+        f.write(data['image'].read())
+        f.close()
+
+    def update_rating(self, rating_id, data):
+        rating_id = data['id']
+
+        try:
+            rating = Rating.objects.filter(id=rating_id)[0]
+        except Exception as e:
+            raise e
+
+        rating.rating = data['rating']
+        rating.title = data['title']
+        rating.text = data['text']
+
+        rating.save()
+
+    def delete_rating(self, rating_id, data):
+        rating_id = data['id']
+
+        try:
+            rating = Rating.objects.filter(id=rating_id)[0]
+        except Exception as e:
+            raise e
+
+        rating.delete()
